556e51dc104eb3bec22c3f9fc213508b8b982805
Camden Blatchly <camden.blatchly@ruralinnovation.us>
2022-03-02 15:48:44 -0500

    added second dataset and example

--- sample_02_remote_work_by_education.R
+++ sample_02_remote_work_by_education.R
@@ -5,5 +5,20 @@ library(ggplot2)
 # over time by education level
 
 # Read in sample data
-map48_sample_data <- readr::read_csv("data/time_series_sample_data.csv")
+map48_sample_data <- readr::read_csv("data/time_series_sample_data.csv", col_types = list(readr::col_date(format = "%m/%d/%y")))
 
+# Format for chart consumption
+remote_work_by_ed_level <- map48_sample_data %>%
+  tidyr::pivot_longer(
+    cols = !date,
+    names_to = "education_level",
+    values_to = "pct_working_remotely"
+  )
+
+# ggplot with default styling
+fig <- remote_work_by_ed_level %>%
+  ggplot(aes(date, pct_working_remotely, group = education_level, color = education_level)) +
+  geom_line() +
+  xlab("")
+
+fig
